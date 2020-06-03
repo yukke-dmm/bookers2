9fbfba6fef1762073b981582b79e0ba4d5fcc9cb
森田悠介 <yusuke@moritayuusukenoMacBook-Air.local>
2020-06-04 01:19:31 +0900

    詳細ページまで

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'books#index'
+
+  resources :books, only: [:new, :create, :index, :show]
+
+end
